2a4d293909234af8b05442b848dc9d10a0bd7b10
Mohammad <mohammad.dahamshi@gmail.com>
2025-05-18 19:49:57 +0300

    09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)
+     || a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    let min = Math.min(a,b);
+    let max = Math.max(a,b);
+    return ((max - min + 1) * (min + max)) / 2; //sequence sum
 };
 
 // Do not edit below this line
